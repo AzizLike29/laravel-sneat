52a88c60d141078a80d42b83efbef109f3cd798e
AzizLike29 <abdulfirdaus590@gmail.com>
2025-03-07 14:38:47 +0700

    first commit

---
+++ lang/en/field.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Field Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Field of menu or table's column on database
+    |
+    */
+
+    // Users
+    'name' => 'Name',
+    'email' => 'Email',
+    'password' => 'Password',
+    'password_confirmation' => 'Password Confirmation',
+    'current_password' => 'Current Password',
+    'role' => 'Role',
+    'permission' => 'Permission',
+    'module' => 'Module',
+    'description' => 'Description',
+    'created_at' => 'Created At',
+    'updated_at' => 'Updated At',
+    'deleted_at' => 'Deleted At',
+    'timestamp' => 'Timestamp',
+];
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
+        <div class="mb-2 mb-md-0">
+            © 2025, made with ❤️ by
+            <a href="https://github.com/AzizLike29" target="_blank" class="footer-link fw-medium">Abdul Aziz
+                Firdaus</a>
+        </div>
+        <div class="d-none d-lg-inline-block">
+            <a href="https://github.com/404NotFoundIndonesia/laravel-11-sneat-template/blob/main/LICENSE"
+                class="footer-link me-4" target="_blank">License</a>
+            <a href="https://github.com/404NotFoundIndonesia/laravel-11-sneat-template#get-started" target="_blank"
+                class="footer-link me-4">Documentation</a>
+        </div>
+    </div>
+</footer>
